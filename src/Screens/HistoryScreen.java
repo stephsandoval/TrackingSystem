21ec0513288013f57d8611283cff3dd2779ce531
Stephanie Sandoval <stephsandovalc@gmail.com>
2023-10-21 13:07:28 -0600

    history screen logic

--- src/Screens/HistoryScreen.java
+++ src/Screens/HistoryScreen.java
@@ -1,5 +1,23 @@
 package Screens;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class HistoryScreen extends ScreenChanger{
     
+    @FXML
+    private TextField screenPackageID;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private TableView <String> historyTable;
+
+    public void searchID (){
+        int packageID = Integer.valueOf(screenPackageID.getText());
+        // send id to the controller to check
+        // expects something from the controller related to whether the package exists
+        // if the package exists, show history in table
+    }
 }
